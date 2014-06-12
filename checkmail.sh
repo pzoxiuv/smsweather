b7db76938770b74d8debe94ba65da69f655f5424
Alex Merenstein <alex.merenstein@gmail.com>
2014-06-11 21:14:15 -0400

    Updates shell script to log output somewhere and not run Lua scripts if there aren't any new emails.

--- checkmail.sh
+++ checkmail.sh
@@ -2,8 +2,10 @@
 
 getmail
 
-for f in /home/alex/Programming/smsweather/mail/new/*
-do
-	lua /home/alex/Programming/smsweather/smsweather.lua $f
-	rm $f
-done
+if [ "$(ls -A /home/alex/Programming/smsweather/mail/new)" ]; then
+	for f in /home/alex/Programming/smsweather/mail/new/*
+	do
+		lua /home/alex/Programming/smsweather/smsweather.lua $f &>>/home/alex/Programming/smsweather/luaerrs
+		rm $f
+	done
+fi
